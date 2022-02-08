7404fc40cbbd870e8cfcf50fd7ccb409d660186c
Cecer <cecer@hypixel.net>
2022-02-08 20:26:37 +0000

    Fix transformation canvas parent logic

--- src/main/java/com/cecer1/projects/mc/cecermclib/fabric/modules/rendering/context/transformations/AbstractTransformationCanvas.java
+++ src/main/java/com/cecer1/projects/mc/cecermclib/fabric/modules/rendering/context/transformations/AbstractTransformationCanvas.java
@@ -1,12 +1,12 @@
 package com.cecer1.projects.mc.cecermclib.fabric.modules.rendering.context.transformations;
 
 import com.cecer1.projects.mc.cecermclib.fabric.modules.rendering.context.AbstractCanvas;
+import com.cecer1.projects.mc.cecermclib.fabric.modules.rendering.context.AbstractStandardCanvas;
 import com.cecer1.projects.mc.cecermclib.fabric.modules.rendering.context.RenderContext;
 
 /**
  * A simplified canvas that is not intended to be applied directly but rather a result of a compound canvas.
- * Unlike an AbstractStandardCanvas canvas, this should generally not pop or push states and, in many cases, no unapply method is even required as a result.
- * The apply() also does not return a CanvasCloser.
+ * Unlike an {@link AbstractStandardCanvas} canvas, this should generally not pop or push states and, in many cases, no unapply method is even required as a result.
  */
 public abstract class AbstractTransformationCanvas extends AbstractCanvas {
 
