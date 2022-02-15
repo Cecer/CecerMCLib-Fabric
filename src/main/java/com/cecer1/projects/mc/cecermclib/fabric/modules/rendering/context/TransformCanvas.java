8415862ef35bb85d385de38e35c89796a5e50072
Cecer <cecer@hypixel.net>
2022-02-15 22:22:40 +0000

    Fix transforms ignoring transformations

--- src/main/java/com/cecer1/projects/mc/cecermclib/fabric/modules/rendering/context/TransformCanvas.java
+++ src/main/java/com/cecer1/projects/mc/cecermclib/fabric/modules/rendering/context/TransformCanvas.java
@@ -14,6 +14,60 @@ public class TransformCanvas extends AbstractStandardCanvas {
         this.transformations = transformations;
     }
 
+    private AbstractCanvas getLastTransformationOrParent() {
+        return this.transformations.length == 0 ? this.parentCanvas : this.transformations[transformations.length - 1];
+    }
+
+    @Override
+    public float getTrueScale() {
+        return this.getLastTransformationOrParent().getTrueScale();
+    }
+
+    @Override
+    public int getTrueX() {
+        return this.getLastTransformationOrParent().getTrueX();
+    }
+
+    @Override
+    public int getTrueY() {
+        return this.getLastTransformationOrParent().getTrueY();
+    }
+
+    @Override
+    public int getTrueWidth() {
+        return this.getLastTransformationOrParent().getTrueWidth();
+    }
+
+    @Override
+    public int getTrueHeight() {
+        return this.getLastTransformationOrParent().getTrueHeight();
+    }
+
+    @Override
+    public int getRelativeX() {
+        return this.getLastTransformationOrParent().getRelativeX();
+    }
+
+    @Override
+    public int getRelativeY() {
+        return this.getLastTransformationOrParent().getRelativeY();
+    }
+
+    @Override
+    public int getWidth() {
+        return this.getLastTransformationOrParent().getWidth();
+    }
+
+    @Override
+    public int getHeight() {
+        return this.getLastTransformationOrParent().getHeight();
+    }
+
+    @Override
+    public boolean isAbsoluteTrueCoordsWithin(int x, int y) {
+        return this.getLastTransformationOrParent().isAbsoluteTrueCoordsWithin(x, y);
+    }
+
     @Override
     public AbstractStandardCanvas open() {
         AbstractStandardCanvas superResult = super.open();
